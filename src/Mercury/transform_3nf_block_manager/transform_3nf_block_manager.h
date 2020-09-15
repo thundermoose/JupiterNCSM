ae20771024848f0db8a6496547064744e8791c6b
Thundermoose <tordjarv@gmail.com>
2020-09-15 09:20:53 +0200

    Header file for transform_3nf_block_manager
    Wrote the header of the transform_3nf_block_manager, based on the design
    for the 2nf version.

---
+++ src/Mercury/transform_3nf_block_manager/transform_3nf_block_manager.h
@@ -0,0 +1,21 @@
+#ifndef __TRANSFORM_3NF_BLOCK_MANAGER__
+#define __TRANSFORM_3NF_BLOCK_MANAGER__
+
+struct _tranform_2nf_block_manager_;
+typedef struct _tranform_2nf_block_manager_ *transform_3nf_block_manager_t;
+
+transform_3nf_block_manager_t 
+new_transform_3nf_block_manager(Data_File *coupled_3nf_data,
+				const char *index_list_path,
+				const int single_particle_energy);
+
+void decouple_transform_3nf_block(transform_3nf_block_manager_t manager,
+				  transform_block_settings_t block);
+
+mercury_matrix_block_t
+get_transform_3nf_matrix_block(transform_3nf_block_manager_t manager,
+			       matrix_block_setting_t block)
+
+void free_transform_3nf_block_manager(transform_3nf_block_manager_t manager);
+
+#endif
