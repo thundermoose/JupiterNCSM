1a9fb5327010963fe738c5289c6d34d8b6b8979f
Thundermoose <tordjarv@gmail.com>
2020-09-16 20:54:20 +0200

    Redirected interaction tests data path

--- src/Mercury/interaction/interaction_test.c
+++ src/Mercury/interaction/interaction_test.c
@@ -3,9 +3,10 @@
 #include <log/log.h>
 #include <math.h>
 
+#define TWO_NUC_FORCES "two_nucleon_force_data/m_scheme/"
 new_test(finding_pp_diagonal_state,
 	 const char *interaction_path = 
-	 TEST_DATA "n2lo_sat_2nf_hw20_Nmax2_noho";
+	 TEST_DATA TWO_NUC_FORCES "n2lo_sat_2nf_hw20_Nmax2_noho";
 	 interaction_t interaction = new_interaction(interaction_path);
 	 int bra_state[2] = {2,14};
 	 int ket_state[2] = {2,14};
@@ -23,7 +24,7 @@ new_test(finding_pp_diagonal_state,
 
 new_test(finding_np_diagonal_state,
 	 const char *interaction_path = 
-	 TEST_DATA "n2lo_sat_2nf_hw20_Nmax4_noho";
+	 TEST_DATA TWO_NUC_FORCES "n2lo_sat_2nf_hw20_Nmax4_noho";
 	 interaction_t interaction = new_interaction(interaction_path);
 	 int bra_state[2] = {1,6};
 	 int ket_state[2] = {23,12};
