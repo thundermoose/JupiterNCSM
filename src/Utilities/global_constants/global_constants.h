046336e389bc7a586bc2932fe66f1a3896ec6114
Thundermoose <tordjarv@gmail.com>
2020-09-17 09:24:52 +0200

    Fixed a problem with older versions of gcc

--- src/Utilities/global_constants/global_constants.h
+++ src/Utilities/global_constants/global_constants.h
@@ -1,8 +1,16 @@
 #ifndef __GLOBAL_CONSTANTS__
 #define __GLOBAL_CONSTANTS__
 
-#define max_interaction_order 3
+#if __GNUC__ > 6
+static const size_t max_interaction_order = 3;
 
 static const size_t no_index = -1;
+#else
+
+#define max_interaction_order 3
+
+#define no_index (size_t)(-1)
+
+#endif
 
 #endif
