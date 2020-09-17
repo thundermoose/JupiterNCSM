87a799beda1e1affbfe68ce3872dd23e03907d3a
Thundermoose <tordjarv@gmail.com>
2020-09-17 09:27:23 +0200

    In case we compile with something that is not gcc

--- src/Utilities/global_constants/global_constants.h
+++ src/Utilities/global_constants/global_constants.h
@@ -1,15 +1,17 @@
 #ifndef __GLOBAL_CONSTANTS__
 #define __GLOBAL_CONSTANTS__
 
-#if __GNUC__ > 6
-static const size_t max_interaction_order = 3;
-
-static const size_t no_index = -1;
-#else
+#if __GNUC__ <= 6
 
 #define max_interaction_order 3
 
 #define no_index (size_t)(-1)
+
+#else
+
+static const size_t max_interaction_order = 3;
+
+static const size_t no_index = -1;
 
 #endif
 
