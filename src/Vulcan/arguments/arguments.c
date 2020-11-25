99da4a9748f7ce4621badcc0615fa956e65b7835
Thundermoose <tordjarv@gmail.com>
2020-11-25 09:13:23 +0100

    A code to add interactions

---
+++ src/Vulcan/arguments/arguments.c
@@ -0,0 +1,122 @@
+#include <arguments/arguments.h>
+
+struct _arguments_
+{
+	int usage;
+	char *program;
+	char *combination_table_path;
+	size_t num_protons;
+	size_t num_neutrons;
+	size_t num_operators;
+	double *coefficients;
+	char **operator_paths;
+	char *output_path;	
+};
+
+const double one = 1.0;
+
+arguments_t parse_arguments(size_t num_arguments,
+			    char **argument_list)
+{
+	arguments_t arguments =
+	       	(arguments_t)calloc(1,sizeof(struct _arguments_));
+	arguments->program = *argument_list;
+	if (num_arguments < 5)
+	{
+		arguments->usage = 1;
+		return arguments;
+	}
+	else
+	{
+		arguments->usage = 0;
+		arguments->output_path = argument_list[1];
+		arguments->combination_table_path = argument_list[2];
+		arguments->num_protons = atoll(argument_list[3]);
+		arguments->num_neutrons = atoll(argument_list[4]);
+		array_builder_t operator_paths_builder = 
+			new_array_builder(&arguments->operator_paths,
+					  &arguments->num_operators,
+					  sizeof(char*));
+		size_t num_coefficients = 0;
+		array_builder_t coefficient_builder =
+			new_array_builder(&arguments->coefficients,
+					  &num_coefficients,
+					  sizeof(double));
+		for (size_t i = 5; i<num_arguments; i++)
+		{
+			if (strcmp(argument_list[i],
+				   "-c") == 0 ||
+			    strcmp(argument_list[i],
+				   "--coefficient") == 0)
+			{
+				arguments->coefficients[num_coefficients-1] =
+					atof(argument_list[++i]);
+			}
+			else
+			{
+				append_array_element(operator_paths_builder,
+						     argument_list[i]);
+				append_array_element(coefficient_builder,
+						     &one);
+			}
+		}
+		free_array_builder(operator_paths_builder);
+		free_array_builder(coefficient_builder);
+		return arguments;
+	}
+}
+
+int should_show_usage(arguments_t arguments)
+{
+	return arguments->usage;
+}
+
+void show_usage(arguments_t arguments)
+{
+	printf("Usage: %s <output_path> <comb.txt> "
+	       "<num protons> <num neutrons> "
+	       "[operator_path [-c/--coefficient <value>]]...",
+	       arguments->program);
+}
+
+const char *get_combination_table_argument(arguments_t arguments)
+{
+	return arguments->combination_table_path;
+}
+
+size_t get_num_protons_argument(arguments_t arguments)
+{
+	return arguments->num_protons;
+}
+
+size_t get_num_neutron_argument(arguments_t arguments)
+{
+	return arguments->num_neutrons;
+}
+
+size_t num_operator_arguments(arguments_t arguments)
+{
+	size_t arguments->num_operators;
+}
+
+const double *get_coefficient_arguments(arguments_t arguments)
+{
+	return arguments->coefficients;
+}
+
+const char **get_operator_path_arguments(arguments_t arguments)
+{
+	return arguments->operator_paths;
+}
+
+const char *get_output_path_argument(arguments_t arguments)
+{
+	return arguments->output_path;
+}
+
+void free_arguments(arguments_t arguments)
+{
+	free(arguments->coefficients);
+	free(arguments->operator_paths);
+	free(arguments);
+}
--- src/Vulcan/programs/vulcan.c
+++ src/Vulcan/programs/vulcan.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <omp.h>
+#include <string.h>
+#include <matrix_block_setting/matrix_block_setting.h>
+#include <arguments/arguments.h>
+#include <error/error.h>
 
 static
 void add_operator_blocks(matrix_block_setting_t block,
@@ -9,6 +13,18 @@ void add_operator_blocks(matrix_block_setting_t block,
 			 size_t num_operators,
 			 char *output_path,
 			 double *workspace);
+
+static
+void load_operator(double *buffer,
+		   char *operator_path,
+		   size_t matrix_block_id,
+		   size_t block_size);
+
+static
+void save_operator(double *buffer,
+		   char *operator_path,
+		   size_t matrix_block_id,
+		   matrix_block_setting_t block);
 
 int main(int num_arguments,
 	 char **argument_list)
@@ -40,7 +56,7 @@ int main(int num_arguments,
 	size_t num_operators = num_operator_arguments(arguments);
 	char *output_path = get_output_path_argument(arguments);
 	size_t workspace_size_per_thread =
-	       	max_needed_block_size*(num_operators+1);
+	       	max_needed_block_size*2;
 	double *total_workspace = NULL;
 	size_t total_workspace_size;
 	reset_matrix_block_iterator(combination_table);
@@ -86,4 +102,81 @@ void add_operator_blocks(matrix_block_setting_t block,
 {
 	size_t block_size = block.num_proton_combinations*
 		block.num_neutron_combinations;
+	double *output_block = workspace;
+	double *input_block = workspace+block_size;
+	memset(output_block,0,block_size*sizeof(double));
+	for (size_t i = 0; i<num_operators; i++)
+	{
+		load_operator(input_block,
+			      operator_paths[i],
+			      block.matrix_block_id,
+			      block_size);
+		double coefficient = coefficients[i];
+		for (size_t j = 0; j < block_size; j++)
+			input_block[j]+=coefficient*output_block[j];
+	}
+	save_operator(input_block,
+		      output_path,
+		      block.matrix_block_id,
+		      block);
+}
+
+static
+void load_operator(double *buffer,
+		   char *operator_path,
+		   size_t matrix_block_id,
+		   size_t block_size)
+{
+	char filename_buffer[1024];
+	sprintf(filename_buffer,
+		"%s/%lu_matrix_elements",
+		operator_path,
+		matrix_block_id);
+	FILE *matrix_file = fopen(filename_buffer,"r");
+	fseek(filename_buffer,2*sizeof(size_t),SEEK_SET);
+	if (fread(buffer,sizeof(double),block_size,matrix_file) != block_size)
+		error("Could not read block %lu from operator %s\n",
+		      matrix_block_id,operator_path);
+	fclose(matrix_file);
+}
+
+static
+void save_operator(const double *buffer,
+		   char *operator_path,
+		   size_t matrix_block_id,
+		   matrix_block_setting_t block)
+{
+	char filename_buffer[1024];
+	sprintf(filename_buffer,
+		"%s/%lu_matrix_elements",
+		operator_path,
+		matrix_block_id);
+	FILE *matrix_file = fopen(filename_buffer,"w");
+	if (fwrite(&block.num_neutron_combinations,
+		   sizeof(size_t),
+		   1,
+		   matrix_file) != 1)
+		error("Could not write neutron dimension to matrix file %lu "
+		      "in operator %s\n",
+		      matrix_block_id,
+		      operator_path);
+	if (fwrite(&block.num_proton_combinations,
+		   sizeof(size_t),
+		   1,
+		   matrix_file) != 1)
+		error("Could not write proton dimension to matrix file %lu "
+		      "in operator %s\n",
+		      matrix_block_id,
+		      operator_path);
+	size_t block_size =
+	       	block.num_proton_combinations*block.num_neutron_combinations;
+	if (fwrite(buffer,
+		   sizeof(double),
+		   block_size,
+		   matrix_file) != block_size)
+		error("Could not write matrix elements to matrix block %lu for"
+		      " operator %s\n",
+		      matrix_block_id,
+		      operator_path);
+	fclose(matrix_file);
 }
